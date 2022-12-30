17ef430584caad37cbae70f0e22cb833b4b2543f
iamSangohan <mensahluc81@gmail.com>
2022-12-30 13:45:56 +0000

    avec les points

--- amion_little.py
+++ amion_little.py
@@ -10,21 +10,21 @@ matrix = [['.',780,320,580,480,660],
 
 def sumMin(matrix:list):
     mat = copy.deepcopy(matrix)
+    # print('------------------------------')
+    # for line in mat:
+        # print(line)
     sumM = 0
     for i in range(len(mat)):
         line = mat[i]
         minimum = 99999999
         for j in range(len(mat[i])):
-            #On recherche le min de chaque ligne de la matrice 
             element = mat[i][j]
             if not isinstance(element,int) :
                 pass
             else:
                 if element<minimum:
                     minimum = element
-        #On additionne tous les min des lignes de la matrice 
         sumM += minimum
-        #On retire le min sur toute la ligne 
         for j in range(len(line)):
             if not isinstance(mat[i][j], int): pass
             else: mat[i][j] -= minimum
@@ -33,7 +33,6 @@ def sumMin(matrix:list):
     # print("................................................")
     for i in range(len(mat)):
         minimum = 999999999
-        #On fait pareil pour les colonnes
         for j in range(len(mat)):
             elementCol = mat[j][i]
             if not isinstance(elementCol, int) :
@@ -51,6 +50,7 @@ def sumMin(matrix:list):
                     # print(mat[j][i])
                     mat[j][i] -= minimum
                     # print(mat[j][i])
+    #print(sumM)
     return sumM,mat
 
 def regret(matrix):
@@ -58,26 +58,33 @@ def regret(matrix):
     regretList = []
     for i in range(len(mat)):
         for j in range(len(mat[i])):
-            if mat[i][j] == 0 :
-                #On met tous les elements de la ligne dans un tableau puis on prend le min
+            if mat[i][j] != 0 : pass
+            else:
                 line = mat[i].copy()
                 line.remove(mat[i][j])
                 line = [x for x in line if x != '.']
-                minLine = min(line)
-                #On met tous les elements de la colonne dans un tableau puis on prend le min
-                col = [mat[a][j] for a in range(len(matrix))] 
+                if len(line)==0:
+                    minLine = 0
+                else:
+                    minLine = min(line)
+                col = [mat[a][j] for a in range(len(matrix))]
                 col.remove(mat[i][j])
-                col = [x for x in col if x != '.']
-                minCol = min(col)
+                col = [x for x in col if x!='.']
+                if len(col)==0:
+                    minCol = 0
+                else:
+                    # print(f'mat\n  {mat} \n \n col {col} \n \n ')
+                    minCol = min(col)
                 regretList.append([i,j,minLine+minCol])
-    max_value = 0
-    regretMax = []
-    #print(regretList)
+    max = 0
     for element in regretList:
-        if element[2] > max_value:
+        if element[2] >= max:
             regretMax = element
-            max_value = regretMax[2]
-    print(regretMax)      
+            max = regretMax[2]
+    if len(mat[0])>2:
+        mat[regretMax[1]][regretMax[0]] = '.'
+        # print(f'chemin inv\n  {mat}\n \n  regret {regretList} \n MAx')
+    
     #Suppression de la ligne et la colonne du regret
     col = regretMax[1]
     line = regretMax[0]
@@ -89,35 +96,29 @@ def regret(matrix):
         for j in range(len(matReduit[i])):
             if j == col:
                 matReduit[i][j] = "."
+            
+    return regretMax,matReduit
     
-    return regretMax, mat
+def little(matrix : list, val = 0):
+    sumM,newMat = sumMin(matrix)
+    regretMax,Mat = regret(newMat)
+    print('-----------------')
+    print('val :',val)
+    print('matrice :',matrix)
+    racine = sumM
+    if regretMax[2] != 0:
+        gauche = [regretMax[0],regretMax[1],racine+regretMax[2],False]    
+        tree = [gauche,racine,[regretMax[0],regretMax[1],little(Mat,racine),True]]
+        print(tree)
+    
+    
+    
+    
+    # return newMat,tree,val
 
-mat = sumMin(matrix)[1]
-print(regret(mat)[1])
-
-def little(matrix : list):
-    ## tree = [g,r,d]
-    tree = []
-    t = {}
-    val,newMat = sumMin(matrix)
-    regretMax = regret(newMat)
-    tree["valeur"]
-
-    t[str(regretMax[0])+str(regretMax[1])+'_'] = val+regretMax[2]
-    tree[val] = [t,tree]
-    # tree.append([[val+regretMax[2]],val,tree])
-    print(tree)
-
+# def recurLittle(matrix:list)->list:
+#     tree = []
+#     recurLittle(matrix, tree,0)
+        
 little(matrix)
 
-# def ajouter_noeud(arbre, valeur):
-#     if arbre is None:
-#         arbre = {}
-#         arbre['valeur'] = valeur #Valeur va etre un tableau avec les sommets et la valeur du trajet
-#         arbre['gauche'] = None
-#         arbre['droit'] = None
-#     elif valeur < arbre['valeur']:
-#         arbre['gauche'] = ajouter_noeud(arbre['gauche'], valeur)
-#     else:
-#         arbre['droit'] = ajouter_noeud(arbre['droit'], valeur)
-#     return arbre
